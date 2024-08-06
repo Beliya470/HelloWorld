a3e215b854f4a991a5d2bb7decf85b8fd14aea2b
Beliya470 <anneanziya78@gmail.com>
2024-08-06 10:59:32 +0300

    Added PrimitiveTypes.java

---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte age = 30;
+        int viewsCount = 123_456_789;
+        long viewsCountLong = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(viewsCountLong);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(isEligible);
+    }
+}
