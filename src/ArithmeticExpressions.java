2959bd2a0510da8858b231ff3011fc00f82e14c7
Beliya470 <anneanziya78@gmail.com>
2024-08-06 11:12:26 +0300

    Added ArithmeticExpressions.java

---
+++ src/ArithmeticExpressions.java
@@ -0,0 +1,18 @@
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+        int result = 10 + 3;
+        System.out.println(result);
+
+        result = 10 - 3;
+        System.out.println(result);
+
+        result = 10 * 3;
+        System.out.println(result);
+
+        result = 10 / 3;
+        System.out.println(result);
+
+        result = 10 % 3;
+        System.out.println(result);
+    }
+}
