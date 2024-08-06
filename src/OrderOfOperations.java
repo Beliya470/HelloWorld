325e291c2b370fe5158978512f5c56c4102c5f19
Beliya470 <anneanziya78@gmail.com>
2024-08-06 11:13:31 +0300

    Added OrderOfOperations.java

---
+++ src/OrderOfOperations.java
@@ -0,0 +1,9 @@
+public class OrderOfOperations {
+    public static void main(String[] args) {
+        int result = 10 + 3 * 2;
+        System.out.println(result);
+
+        result = (10 + 3) * 2;
+        System.out.println(result);
+    }
+}
