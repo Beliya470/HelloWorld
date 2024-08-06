37bb02eb449c417c1ba2b7af4398a4c9e24fbe2d
Beliya470 <anneanziya78@gmail.com>
2024-08-06 11:10:03 +0300

    Added MultiDimensionalArrays.java

---
+++ src/MultiDimensionalArrays.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class MultiDimensionalArrays {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
