08b39102e0a4a514866765c41103cfa9d680e1c9
Beliya470 <anneanziya78@gmail.com>
2024-08-06 10:57:47 +0300

    Added Variables class

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 30;
+        int temperature = 20;
+        int myAge = 30;
+        int herAge = myAge;
+
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println(herAge);
+    }
+}
