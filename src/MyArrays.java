20453aa73fa45d994cb6a3b905e541367d96e7cf
Beliya470 <anneanziya78@gmail.com>
2024-08-06 11:09:11 +0300

    Added Arrays.java

---
+++ src/MyArrays.java
@@ -0,0 +1,7 @@
+public class MyArrays {
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 5, 1, 4};
+        java.util.Arrays.sort(numbers);
+        System.out.println(java.util.Arrays.toString(numbers));
+    }
+}
